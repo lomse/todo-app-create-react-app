1d3352989451612d96fb337673c07d58a900efbb
Selom <shellom2005@gmail.com>
2018-01-17 23:04:57 +0000

    added a delete todo

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -4,14 +4,19 @@ import { partial } from '../../lib/utils';
 
 export const TodoItem = props => { 
     const hangleToggle = partial(props.handleToggle, props.id)
+    const handleRemove = partial(props.handleRemove, props.id)
 
     return (
         <li>
             <label htmlFor={props.id}>
                 <input type="checkbox" id={props.id}
                     checked={props.isComplete}
-                    onChange={ hangleToggle }    
-                /> {props.name}</label>
+                    onChange={hangleToggle}   
+                    
+                />
+                {props.name}
+            </label> 
+            <a href="#" title="Remove Todo" onClick={handleRemove}>x</a>    
             </li>
     )
 }
